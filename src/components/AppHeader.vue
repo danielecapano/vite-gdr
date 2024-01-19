df9e96ad952eecc769ca24db7bbe5d1185b9091e
SinghAmandeep <amandeepsingh3095@gmail.com>
2024-01-19 17:54:41 +0100

    add header and show api

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,14 @@
+<script></script>
+
+<template>
+    <header>
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li><router-link :to="{ name: 'characters.index' }">Characters</router-link></li>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+<style lang="scss" scoped></style>
--- src/pages/characters/Show.vue
+++ src/pages/characters/Show.vue
@@ -1,5 +1,40 @@
-<script></script>
+<script>
+import axios from 'axios';
 
-<template></template>
+
+export default {
+    props: ['id'],
+    data() {
+        return {
+            character: null,
+            BASE_URL: 'http://127.0.0.1:8000/api',
+        }
+    },
+    methods: {
+        fetchCharacter() {
+
+            axios.get(`${this.BASE_URL}/characters`, {
+                params: {
+                    id: this.id
+                }
+            })
+                .then(res => {
+                    this.character = res.data.results;
+                    console.log(this.character);
+                });
+        }
+    },
+    created() {
+        this.fetchCharacter()
+    }
+}
+</script>
+
+<template>
+    <ul>
+        <li>{{ id }}</li>
+        <li></li>
+    </ul>
+</template>
 
 <style lang="scss" scoped></style>
